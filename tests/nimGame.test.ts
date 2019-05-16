0a0ed60f96502110bc36f120e4bd934cbbf9d90b
Julius F. Martin <julius.martin@jfm-ma.de>
2019-05-16 14:45:14 +0200

    lint fixed tests

--- tests/nimGame.test.ts
+++ tests/nimGame.test.ts
@@ -1,9 +1,8 @@
 import test from 'ava';
-import config from 'config'
 import NimGame from '../src/nimGame';
 
-test('Start situation is loaded', t => {
-    const startSituation = [1,3,5,7];
+test('Start situation is loaded', (t) => {
+    const startSituation = [1, 3, 5, 7];
 
     const game = new NimGame(1, startSituation);
     const status = game.getStatus();
@@ -11,25 +10,25 @@ test('Start situation is loaded', t => {
     t.is(status.stacks.join(), startSituation.join());
 });
 
-test('Start situation balance is correct', t => {
-    const game0 = new NimGame(1, [1,3,5,7]);
-    const game1 = new NimGame(1, [1,3,5,6]);
+test('Start situation balance is correct', (t) => {
+    const game0 = new NimGame(1, [1, 3, 5, 7]);
+    const game1 = new NimGame(1, [1, 3, 5, 6]);
 
     t.true(game0.isBalanced());
     t.false(game1.isBalanced());
 });
 
-test('Computer can balance 1000 situation', t => {
-    for (let i = 0; i<10000; i++) {
-        const randomArray = [0,0,0,0].map(() => Math.round(Math.random() * 10));
-        let game = new NimGame(1, randomArray);
+test('Computer can balance 1000 situation', (t) => {
+    for (let i = 0; i < 10000; i++) {
+        const randomArray = [0, 0, 0, 0].map(() => Math.round(Math.random() * 10));
+        const game = new NimGame(1, randomArray);
         game.makeBalanced();
         // console.log(`Testing situation [${randomArray.join(', ')}] -> [${game.getStatus().stacks.join(', ')}]`);
         t.true(game.isBalanced());
     }
 });
 
-test('Play a game till its over', t => {
+test('Play a game till its over', (t) => {
     const game = new NimGame(1, [1, 3, 5, 7, 9, 11, 13 , 15, 17]);
 
     while (!game.isGameOver()) {
